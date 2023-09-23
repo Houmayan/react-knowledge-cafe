4eb9dd243333dfec2aad73a9437860be5195b05f
houmayan <alichy101@gmail.com>
2023-09-23 11:08:56 +0600

    project initialised

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-amber-500'>Vite + React</h1>
+      <p className='text-2xl'>Knowledge Cafe</p>
+      
+    </>
+  )
+}
+
+export default App
