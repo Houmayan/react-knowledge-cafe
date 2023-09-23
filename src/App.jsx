73756d34146af2797679bb017a228a8147804e9d
houmayan <alichy101@gmail.com>
2023-09-23 11:27:59 +0600

    json data added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +10,7 @@ function App() {
   return (
     <>
       
-      <h1 className='text-6xl bg-amber-500'>Vite + React</h1>
-      <p className='text-2xl'>Knowledge Cafe</p>
+     <Header></Header>
       
     </>
   )
